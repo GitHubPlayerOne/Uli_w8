ea68faf0fa1ca05cec45b65f19ec090126653056
Uli <ru21041555@gmail.com>
2023-09-10 01:08:59 +0800

    Updates

---
+++ assets/product-vito-1566f6c0.js
@@ -0,0 +1,1 @@
+import"./pagination-efa2f810.js";import{S as e}from"./swiper-bundle-ae89c236.js";new e(".swiper-product",{slidesPerView:1,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
