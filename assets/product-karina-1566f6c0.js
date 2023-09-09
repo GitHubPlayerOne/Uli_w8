97324e9685579980b409475371b201c3fdd9c83b
Uli <ru21041555@gmail.com>
2023-09-10 04:04:30 +0800

    Updates

--- assets/product-karina-1566f6c0.js
+++
@@ -1,1 +0,0 @@
-import"./pagination-efa2f810.js";import{S as e}from"./swiper-bundle-ae89c236.js";new e(".swiper-product",{slidesPerView:1,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
---
+++ assets/product-karina-27877172.js
@@ -0,0 +1,1 @@
+import"./pagination-516dd8b8.js";import{S as e}from"./swiper-bundle-ae89c236.js";new e(".swiper-product",{slidesPerView:1,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
