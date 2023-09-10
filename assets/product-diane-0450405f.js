230743a6a4f3bc6c45048fc04bd7469e7287472c
Uli <ru21041555@gmail.com>
2023-09-10 21:35:42 +0800

    Updates

--- assets/product-diane-0450405f.js
+++
@@ -1,1 +0,0 @@
-import"./pagination-5c1589b6.js";import{S as e}from"./swiper-bundle-ae89c236.js";new e(".swiper-product",{slidesPerView:1,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
---
+++ assets/product-diane-43769291.js
@@ -0,0 +1,1 @@
+import"./pagination-7691947c.js";import{S as e}from"./swiper-bundle-ae89c236.js";new e(".swiper-product",{slidesPerView:1,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
