cd5f97fba2638ab0eb86ed7afb16ec475ebd7e11
Uli <ru21041555@gmail.com>
2023-09-10 17:58:13 +0800

    Updates

---
+++ assets/about-c11565ba.js
@@ -0,0 +1,1 @@
+import"./pagination-5c1589b6.js";import{S as e}from"./swiper-bundle-ae89c236.js";new e(".swiper-about",{spaceBetween:40,slidesPerView:"1",autoplay:{delay:3e3},loop:!0,breakpoints:{768:{slidesPerView:"2"},992:{slidesPerView:"3"}}});
